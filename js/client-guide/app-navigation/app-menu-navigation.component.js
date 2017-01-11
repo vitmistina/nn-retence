fc05f275eee13e30f7c69cb692d1f28982837cad
Vit Mistina <vit.mistina@gmail.com>
2017-01-11 09:47:33 +0100

    switched lib management to bower, employed Grunt for build process

---
+++ js/client-guide/app-navigation/app-menu-navigation.component.js
@@ -0,0 +1,11 @@
+(function () {
+'use strict';
+
+angular.module('clientGuide')
+.component('appMenuNavigation',{
+  templateUrl: 'js/client-guide/app-navigation/app-menu-navigation.html',
+  controller: 'AppMenuNavigationComponentController',
+  controllerAs: 'ctrl'
+});
+
+})();
---
+++ js/client-guide/client-guide.module.js
@@ -0,0 +1,17 @@
+(function() {
+"use strict";
+/**
+ * clientGuide application, includes the ui-router.
+ */
+angular.module('clientGuide',
+  ['ui.router',
+  'ui.bootstrap',
+  'LocalStorageModule',
+  'rzModule',
+  'chart.js',
+  'countUpModule']);
+
+
+
+
+})();
