786cfa12d97efccabe34ec442afaf874dee71a2c
Vit Mistina <vit.mistina@gmail.com>
2021-01-13 12:16:56 +0100

    improvements to calculations

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,124 +1,135 @@
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     jshint: {
       options: {
-        reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+        reporter: require("jshint-stylish"), // use jshint-stylish to make our errors look and read good
       },
       // when this task is run, lint the Gruntfile and all js files in src
-      build: ['Gruntfile.js', 'js/client-guide/**/*.js']
+      build: ["Gruntfile.js", "js/client-guide/**/*.js"],
     },
     bower_concat: {
       options: {
-        separator: ';\n'
+        separator: ";\n",
       },
       build: {
         dest: {
-          js: 'dist/js/app.js',
-          css: 'dist/css/app.css'
+          js: "dist/js/app.js",
+          css: "dist/css/app.css",
         },
         mainFiles: {
-         'angular-i18n': 'angular-locale_cs-cz.js',
-         'angular-slider': 'slider.js',
-         'modernizr': 'modernizr-build.js',
-         'bootstrap-css': 'css/bootstrap.min.css',
-         'countUp.js': ['countUp.js','angular-countUp.js']
-        }
-      }
+          "angular-i18n": "angular-locale_cs-cz.js",
+          "angular-slider": "slider.js",
+          modernizr: "modernizr-build.js",
+          "bootstrap-css": "css/bootstrap.min.css",
+          "countUp.js": ["countUp.js", "angular-countUp.js"],
+        },
+      },
     },
     clean: {
       build: {
-        src: ['dist']
-      }
+        src: ["dist"],
+      },
     },
     copy: {
       build: {
         files: [
           {
             expand: true,
-            src: 'index.html',
-            dest: 'dist/'
+            src: "index.html",
+            dest: "dist/",
           },
           {
             expand: true,
-            src: 'js/client-guide/**/*.html',
-            dest: 'dist/'
+            src: "js/client-guide/**/*.html",
+            dest: "dist/",
           },
           {
             expand: true,
-            src: 'fonts/*',
-            dest: 'dist/'
+            src: "fonts/*",
+            dest: "dist/",
           },
           {
             expand: true,
-            src: 'img/favicon.ico',
-            dest: 'dist/'
+            src: "img/favicon.ico",
+            dest: "dist/",
           },
-        ]
-
-      }
+        ],
+      },
     },
     concat: {
       options: {
-        separator: ';\n'
+        separator: ";\n",
       },
       js: {
-        src: ['dist/js/app.js','js/client-guide/**/*.module.js','js/client-guide/**/*.js'],
-        dest: 'dist/js/app.js',
+        src: [
+          "dist/js/app.js",
+          "js/client-guide/**/*.module.js",
+          "js/client-guide/**/*.js",
+        ],
+        dest: "dist/js/app.js",
       },
       css: {
-        src: ['dist/css/app.css','css/main.css'],
-        dest: 'dist/css/app.css',
-      }
+        src: ["dist/css/app.css", "css/main.css"],
+        dest: "dist/css/app.css",
+      },
     },
     uglify: {
       build: {
         files: {
-          'dist/js/app.js': ['dist/js/app.js']
-        }
-      }
+          "dist/js/app.js": ["dist/js/app.js"],
+        },
+      },
     },
     cssmin: {
       build: {
         options: {
-          keepSpecialComments: 0
+          keepSpecialComments: 0,
         },
         files: {
-          'dist/css/app.css': ['dist/css/app.css']
-        }
-      }
+          "dist/css/app.css": ["dist/css/app.css"],
+        },
+      },
     },
     imagemin: {
       build: {
         options: {
-          optimizationLevel: 1
+          optimizationLevel: 1,
         },
-        files: [{
-          expand:true,
-          src: ['img/**/*.{png,jpg,gif}'],
-          dest: 'dist/'
-        }]
-      }
-    }
-
+        files: [
+          {
+            expand: true,
+            src: ["img/**/*.{png,jpg,gif}"],
+            dest: "dist/",
+          },
+        ],
+      },
+    },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-bower-concat');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-bower-concat");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-imagemin");
 
   grunt.registerTask(
-    'build',
-    'Compiles all of the assets and copies the files to the build directory.',
-    [ 'clean', 'jshint', 'bower_concat', 'copy', 'concat:js', 'concat:css', 'uglify', 'cssmin','imagemin']
+    "build",
+    "Compiles all of the assets and copies the files to the build directory.",
+    [
+      "clean",
+      "jshint",
+      "bower_concat",
+      "copy",
+      "concat:js",
+      "concat:css",
+      "uglify",
+      "cssmin",
+      "imagemin",
+    ]
   );
-
-
 };
--- js/client-guide/expected-pension/expected-pension.controller.js
+++ js/client-guide/expected-pension/expected-pension.controller.js
@@ -1,52 +1,55 @@
 (function () {
-'use strict';
+  "use strict";
 
-angular.module('clientGuide')
-.controller('ExpectedPensionController',ExpectedPensionController);
+  angular
+    .module("clientGuide")
+    .controller("ExpectedPensionController", ExpectedPensionController);
 
-ExpectedPensionController.$inject = ['pensionCalculatorService'];
-function ExpectedPensionController(pensionCalculatorService) {
-  var $ctrl = this;
-  $ctrl.averageMonthlyIncome = 25000;
-  $ctrl.yearsOfInsurance = 40;
-  $ctrl.output = {
-    estimatedPension: null,
-    incomeLoweredBy: null
-  };
+  ExpectedPensionController.$inject = ["pensionCalculatorService"];
+  function ExpectedPensionController(pensionCalculatorService) {
+    var $ctrl = this;
+    $ctrl.averageMonthlyIncome = 25000;
+    $ctrl.yearsOfInsurance = 40;
+    $ctrl.output = {
+      estimatedPension: null,
+      incomeLoweredBy: null,
+    };
 
-  $ctrl.incomeSlider = {
-    options: {
-      floor: 10000,
-      ceil: 60000,
-      step: 5000,
-      hideLimitLabels: true,
-      hidePointerLabels: true,
-      enforceRange: false,
-      enforceStep: false,
-      onChange: function () {
-        $ctrl.update();
+    $ctrl.incomeSlider = {
+      options: {
+        floor: 10000,
+        ceil: 100000,
+        step: 5000,
+        hideLimitLabels: true,
+        hidePointerLabels: true,
+        enforceRange: false,
+        enforceStep: false,
+        onChange: function () {
+          $ctrl.update();
+        },
       },
-    }
-  };
-  $ctrl.yearsOfInsuranceSlider = {
-    options: {
-      floor: 20,
-      ceil: 50,
-      step: 1,
-      hideLimitLabels: true,
-      hidePointerLabels: true,
-      enforceRange: false,
-      enforceStep: false,
-      onChange: function () {
-        $ctrl.update();
+    };
+    $ctrl.yearsOfInsuranceSlider = {
+      options: {
+        floor: 20,
+        ceil: 50,
+        step: 1,
+        hideLimitLabels: true,
+        hidePointerLabels: true,
+        enforceRange: false,
+        enforceStep: false,
+        onChange: function () {
+          $ctrl.update();
+        },
       },
-    }
-  };
+    };
 
-  $ctrl.update = function () {
-    $ctrl.output = pensionCalculatorService.calculateExpectedPension($ctrl.averageMonthlyIncome,$ctrl.yearsOfInsurance);
-  };
-  $ctrl.update();
-}
-
+    $ctrl.update = function () {
+      $ctrl.output = pensionCalculatorService.calculateExpectedPension(
+        $ctrl.averageMonthlyIncome,
+        $ctrl.yearsOfInsurance
+      );
+    };
+    $ctrl.update();
+  }
 })();
